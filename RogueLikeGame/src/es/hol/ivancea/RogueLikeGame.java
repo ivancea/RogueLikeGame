8273bac1ae5ee6e58f39bfdd615f58054c546dc1
ivancea <ivancea@mundo-r.com>
2015-11-03 23:12:33 +0100

    Added basic drawing structure

--- RogueLikeGame/src/es/hol/ivancea/RogueLikeGame.java
+++ RogueLikeGame/src/es/hol/ivancea/RogueLikeGame.java
@@ -1,14 +1,17 @@
 package es.hol.ivancea;
 
 import java.awt.Dimension;
+import java.awt.Graphics;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class RogueLikeGame extends JFrame {
+	private static final long serialVersionUID = 1L;
+
+	public static final int WIDTH = 800,
+							HEIGHT = 600;
 	
-	public static final int WINDOW_WIDTH = 800,
-							WINDOW_HEIGHT = 600;
 	private JPanel panel;
 	
 	public RogueLikeGame(){
@@ -16,8 +19,17 @@ public class RogueLikeGame extends JFrame {
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setResizable(false);
 		
-		panel = new JPanel();
-		panel.setPreferredSize(new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT));
+		panel = new JPanel(){
+			private static final long serialVersionUID = 1L;
+			
+			@Override
+			public void paint(Graphics g){
+				g.clearRect(0, 0, this.getWidth(), this.getHeight());
+				paintScene(g);
+			}
+			
+		};
+		panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
 		
 		this.setContentPane(panel);
 		this.pack();
@@ -25,8 +37,14 @@ public class RogueLikeGame extends JFrame {
 		this.setVisible(true);
 	}
 	
+	private void paintScene(Graphics g){
+		// POC
+		g.drawRect(50,50, WIDTH-100, HEIGHT-100);
+	}
+	
 	
 	public static void main(String[] args){
 		new RogueLikeGame();
 	}
 }
+
