db1c1ae0beedf46ef7412045c0c278e0d4c49cfe
ivancea <ivancea@mundo-r.com>
2015-11-03 23:28:36 +0100

    Added simple map and information for start coding

--- RogueLikeGame/src/es/hol/ivancea/RogueLikeGame.java
+++ RogueLikeGame/src/es/hol/ivancea/RogueLikeGame.java
@@ -1,7 +1,10 @@
 package es.hol.ivancea;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -11,6 +14,8 @@ public class RogueLikeGame extends JFrame {
 
 	public static final int WIDTH = 800,
 							HEIGHT = 600;
+	
+	private int[][] map;
 	
 	private JPanel panel;
 	
@@ -31,16 +36,53 @@ public class RogueLikeGame extends JFrame {
 		};
 		panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
 		
+		this.addKeyListener(new KeyListener(){
+			@Override
+			public void keyPressed(KeyEvent arg0) {
+				
+			}
+			@Override
+			public void keyReleased(KeyEvent arg0) {}
+			@Override
+			public void keyTyped(KeyEvent arg0) {}
+			
+		});
+		
 		this.setContentPane(panel);
 		this.pack();
+		
+		this.initialiceGame();
 
 		this.setVisible(true);
 	}
 	
-	private void paintScene(Graphics g){
-		// POC
-		g.drawRect(50,50, WIDTH-100, HEIGHT-100);
+	private void initialiceGame(){
+		map = new int[WIDTH/20][HEIGHT/20];
+		for(int i=0; i<map.length; i++)
+			for(int j=0; j<map[i].length; j++)
+				if(i!=0 && i!=map.length-1
+				&& j!=0 && j!=map[i].length-1)
+					map[i][j] = 0;
+				else
+					map[i][j] = 1;
 	}
+	
+	private void paintScene(Graphics g){
+		for(int i=0; i<map.length; i++)
+			for(int j=0; j<map[i].length; j++){
+				if(map[i][j] == 0)
+					g.setColor(Color.WHITE);
+				else
+					g.setColor(Color.BLACK);
+				g.fillRect(i*20,j*20, 20,20);
+			}
+	}
+	
+	/** TODO:
+	 *  -Add enemies (abstract base class, inherited classes with IA)
+	 *  -Add player
+	 *  -Game moves on player move (key listeners, ¿mouse listeners?)
+	 */
 	
 	
 	public static void main(String[] args){
